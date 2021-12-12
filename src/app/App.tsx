eab851d77eb4155c13343b8fe1e7e38b19330448
milletbard <milletbard@gmail.com>
2021-12-12 23:22:15 +0800

    chrome: initial project

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import "./app.css";
+
+import React from "react";
+import Page from "../pages";
+
+interface IAppProps {}
+
+const App: React.FC<IAppProps> = () => {
+  return <Page />;
+};
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <header>Create React App</header>
+
+      <main>App</main>
+    </div>
+  );
+};
+
+export default Home;
